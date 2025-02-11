478782fc32a171ebbd92ec6bd10bd1e30f8bf9a8
Dimitris <d.alexopoulos@mismatch.gr>
2025-02-11 12:41:20 +0200

    Rename app to Mismatch Streaming

--- android/app/src/main/java/com/streammanagerapp/MainActivity.kt
+++ android/app/src/main/java/com/streammanagerapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "StreamManagerApp"
+  override fun getMainComponentName(): String = "Mismatch Streaming"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
